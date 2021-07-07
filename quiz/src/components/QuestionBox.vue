6f68ece5038c425f5e4bf744c7d00305c0e2ad7c
Filip Papiernik <filippapiernik1999@gmail.com>
2021-07-07 17:09:29 +0200

    Changed the QuestionBox component, Quiz logic
    QuestionBox displays a question and four answers to that question, thanks to the API from 'opentdb.com'. Button Next leading to the next question is working and also shuffled the data from API

--- quiz/src/components/QuestionBox.vue
+++ quiz/src/components/QuestionBox.vue
@@ -2,17 +2,100 @@
     <div>
         <b-jumbotron>
             <template #lead>
-            Some question here?
+            {{ currentQuestion.question }}
             </template>
 
             <hr class="my-4">
 
             <p>
-            List of answers
+                <b-list-group>
+                    <b-list-group-item  
+                    v-for="(answer, index) in answers" 
+                    :key="index"
+                    @click="selectAnswer(index)"
+                    :class="[selectedIndex === index ? 'selected' : '']">
+                        {{ answer }}
+                    </b-list-group-item>
+                </b-list-group>
             </p>
 
             <b-button variant="primary" href="#">Sumbit</b-button>
-            <b-button variant="success" href="#">Next</b-button>
+            <b-button variant="success" @click="nextQuestion" href="#">Next</b-button>
         </b-jumbotron>
     </div>
 </template>
+
+<script>
+import _ from 'lodash'
+export default {
+    // Here the props are being recived from the App.vue
+    // Where this variables/functions are binded to the
+    // QuestionBox template.
+    props: {
+        currentQuestion: Object,
+        nextQuestion: Function
+
+    },
+    data() {
+        return {
+            selectedIndex: null,
+            shuffledAnswers: []
+        }
+    },
+    computed: {
+        answers() {
+            // Copy of an array ... <-
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    watch: {
+        currentQuestion: {
+            immediate: true,
+            handler() {
+                this.selectedIndex = null
+                this.shuffleAnswers()
+            }
+        }
+        // {
+        //     this.selectedIndex = null
+        //     this.shuffleAnswers()
+        // }
+    },
+    methods: {
+        selectAnswer(index) {
+            this.selectedIndex = index
+        },
+        shuffleAnswers() {
+            let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+            this.shuffledAnswers = _.shuffle(answers)
+        }
+    },
+    mounted() {
+        console.log(this.currentQuestion)
+    },
+}
+</script>
+
+<style scoped>
+    .list-group {
+        padding-bottom: 10px;
+    }
+    .btn {
+        margin-right: 10px;
+    }
+    .list-group-item:hover {
+        opacity: 0.5;
+        cursor: pointer;
+    }
+    .selected {
+        background-color: aquamarine;
+    }
+    .correct {
+        background-color: chartreuse;
+    }
+    .incorrect {
+        background-color: red;
+    }
+</style>
